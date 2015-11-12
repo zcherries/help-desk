64755834576718c3118e3ae18980f15e3e8b51c3
William <wpcarro@wustl.edu>
2015-11-12 11:30:38 -0800

    Adds mongoose ODM for db CRUD ops

--- server/server.js
+++ server/server.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var serverHelpers = require('./server-helpers.js');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var app = express();
+
+
+/* -- HOOK UP TO MONGO DB SERVER --*/
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(callback) {
+	// ...
+});
+
 
 // external middleware
 app.use(bodyParser.json());
 
 // for debugging
 app.use(serverHelpers.printReqInfo);
+
+// viewing the db
+app.get('/data', function(req, res, next) {
+	Collection.find(function(err, objects) {
+		res.send(objects);
+	});
+});
 
 // static files
 app.use(express.static('public'));
