33ce699f2d799916f1b5ad3bb17a787c97f3c943
Aram Simonian <aramsimonian91@gmail.com>
2015-11-16 19:12:11 -0800

    working on getting fellow list to render dynamically, currently its hardcoded but soon suppose to make the table to render on fly

---
+++ react fellow list componenet/reactScript.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+   $.getJSON( "./fellowlist.json", function( data ) {
+      var results = [];
+      for(var key in data) {
+         document.write("user with the name " + data[key].fellowName + " has a profile picture as shown here" + "<br>")
+      }
+   });
+});
+
+
+// console.log(results)
+
+// var FellowList = React.createClass({
+//    render: function() {
+//       return (
+//          <div>
+//            <fellow></fellow>
+//          </div>
+//       )
+//    }
+// })
+
+
+var FellowList = React.createClass({
+
+   componentWillMount: function() {
+      console.log('fellow list is ready');
+   },
+   render: function() {
+      return (
+         <p>hi</p>
+      );
+   }
+});
+
+ReactDOM.render(<FellowList />, document.getElementById("result"));
